2105b33d3ee0ac4606aea21c5dd750211e32d6b9
Aloïs Micard <lunamicard@gmail.com>
2020-09-07 08:24:59 +0200

    MOAR test for config

--- internal/opendydnsd/config/config_test.go
+++ internal/opendydnsd/config/config_test.go
@@ -2,7 +2,7 @@ package config
 
 import "testing"
 
-func TestIsValid(t *testing.T) {
+func TestConfig_Valid(t *testing.T) {
 	c := Config{}
 
 	if c.Valid() {
@@ -23,5 +23,71 @@ func TestIsValid(t *testing.T) {
 
 	if !c.Valid() {
 		t.Error("validate() should have work")
+	}
+}
+
+func TestAPIConfig_SSLEnabled(t *testing.T) {
+	c := APIConfig{}
+
+	if c.SSLEnabled() {
+		t.Error()
+	}
+
+	c.Hostname = "foo.bar.baz"
+	if c.SSLEnabled() {
+		t.Error()
+	}
+
+	c.CertCacheDir = "/var/lib/opendydns/certificates"
+	if !c.SSLEnabled() {
+		t.Error()
+	}
+}
+
+func TestAPIConfig_Valid(t *testing.T) {
+	c := APIConfig{}
+
+	if c.Valid() {
+		t.Error()
+	}
+
+	c.ListenAddr = "127.0.0.1:8080"
+	if c.Valid() {
+		t.Error()
+	}
+
+	c.SigningKey = "todo"
+	if !c.Valid() {
+		t.Error()
+	}
+}
+
+func TestDatabaseConfig_Valid(t *testing.T) {
+	c := DatabaseConfig{}
+
+	if c.Valid() {
+		t.Error()
+	}
+
+	c.Driver = "driver"
+	if c.Valid() {
+		t.Error()
+	}
+
+	c.DSN = "test.db"
+	if !c.Valid() {
+		t.Error()
+	}
+}
+
+func TestDomainConfig_String(t *testing.T) {
+	c := DomainConfig{Domain: "example.org"}
+	if c.String() != "example.org" {
+		t.Error()
+	}
+
+	c.Host = "demo"
+	if c.String() != "demo.example.org" {
+		t.Error()
 	}
 }
